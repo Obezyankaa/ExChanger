0554c2db63a721f087b11b467e00e5ac8288df13
Obezyankaa <voloshanowky@yandex.ru>
2022-09-29 17:23:08 +0300

    newfiga

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import ModalLog from './UI/ModalLog';
 import ModalRegistration from './UI/ModalRegistration';
+import Main from './components/Main';
 
 function App() {
+  const [user, setUser] = useState(false);
   return (
     <>
+      <Navbar />
       <ModalRegistration />
       <ModalLog />
+      <Routes>
+        <Route path="/main" element={<Main />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/style-prop-object */
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>
+      <h1>main</h1>
+
+    </div>
+  );
+}
