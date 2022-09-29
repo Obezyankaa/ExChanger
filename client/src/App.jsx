74e0f4d0c902372c55198e120b3dcad9f4907f2a
MikhailBeschastnov <beschastnov06@yandex.ru>
2022-09-29 18:50:33 +0300

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './UI/Navbar';
-import ModalLog from './UI/ModalLog';
-import ModalRegistration from './UI/ModalRegistration';
-import StarUserRating from './UI/StarUserRating';
+import Page404 from './components/pages/Page404';
+import Main from './components/pages/Main';
+import PersonalArea from './components/pages/PersonalArea';
+import AllProduct from './components/pages/AllProduct';
 
 function App() {
-  const [user, setUser] = useState(false);
   return (
     <>
       <Navbar />
-      <ModalRegistration />
-      <ModalLog />
-      <StarUserRating />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/personal-area" element={<PersonalArea />} />
+        <Route path="/all-product" element={<AllProduct />} />
+        <Route path="*" element={<Page404 to="/404" replace />} />
+      </Routes>
+
     </>
   );
 }
