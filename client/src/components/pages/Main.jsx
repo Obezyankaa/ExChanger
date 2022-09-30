efebf6e9200c47dbe3d0cb5c07a23d2796c172cd
camelsport <dmitri.kosunovski@gmail.com>
2022-09-30 14:05:34 +0300

    firstpart

--- client/src/components/pages/Main.jsx
+++ client/src/components/pages/Main.jsx
@@ -9,7 +9,6 @@ export default function Main({
   return (
     <>
       <div>
-        {/* <img src="../../../img/hero.jpg" alt="..." className="img_home" /> */}
         <Slider />
       </div>
       {regActive === true ? (
