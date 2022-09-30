ba200d7a903f701da66b4d71a5b34ea80b17fe03
AynurMv <aynurmotigulov@gmail.com>
2022-09-30 11:45:42 +0300

    Map is done

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -3,18 +3,18 @@ import { load } from '@2gis/mapgl';
 import axios from 'axios';
 import MapWrapper from './MapWrapper';
 
-export default function Map({ address }) {
+export default function Map() {
   useEffect(() => {
     axios.post('/coordinates', { address: 'москва ощепкова 1' })
       .then((resp) => {
         const x = resp.data.lon;
         const y = resp.data.lat;
         const { metro } = resp.data;
-        console.log(resp.data);
+        // console.log(metro);
         let map;
         load().then((mapglAPI) => {
           map = new mapglAPI.Map('map-container', {
-            center: [x ?? 37.59958, y ?? 55.70574],
+            center: [x, y],
             zoom: 13,
             key: 'ee12180b-57c5-4c59-b3a2-6a198e86bf66',
           });
