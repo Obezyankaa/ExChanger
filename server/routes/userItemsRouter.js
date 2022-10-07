0d147bbf10b68d0797dfee254478df2f6cb37c4e
MikhailBeschastnov <beschastnov06@yandex.ru>
2022-10-07 09:49:52 +0300

    fix telega

--- server/routes/userItemsRouter.js
+++ server/routes/userItemsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  Product, ProductPhoto, Category, View,
+  Product, ProductPhoto, Category,
 } = require('../db/models');
 
 const router = express.Router();
