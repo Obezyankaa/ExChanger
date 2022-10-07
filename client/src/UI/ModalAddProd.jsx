6c588fe9dbe583efac8c5f6d53983a5ad2a1d57f
Obezyankaa <voloshanowky@yandex.ru>
2022-10-07 12:29:27 +0300

    flex

--- client/src/UI/ModalAddProd.jsx
+++ client/src/UI/ModalAddProd.jsx
@@ -9,7 +9,7 @@ export default function ModalAddProd({ setAddProdActive }) {
   return (
     <div
       style={{
-        height: '100vh', width: '100%', backgroundColor: 'black', position: 'fixed', top: '0', left: '0', zIndex: '100',
+        height: '100vh', width: '100%', backgroundColor: '0000006e', position: 'fixed', top: '0', left: '0', zIndex: '100',
       }}
       onClick={(e) => {
         e.stopPropagation();
